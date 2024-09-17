f206f142623a7e88816d1eeddba2dc00e9f82c9f
giuseppe costa <giusepe002@gmail.com>
2024-09-17 11:22:12 +0000

    modificação

--- array7.c
+++ array7.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    char nome[]="m";
+    char nome[]="Midoriya";
     int i = 0;
+
+    for(i; i <= 6; i++){
     printf("%c -> %d -> %p\n",nome[i], nome[i], &nome[i]);
+    }
     return 0;
+    
 }
