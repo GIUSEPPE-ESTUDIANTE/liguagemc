6a3c28a41b8864356f79e6eabd663e1f30577567
giuseppe costa <giusepe002@gmail.com>
2024-09-20 11:53:30 +0000

    Estudo de ponteiro

---
+++ src/terceiro_ponteiro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void alterar(int * ponteiro){
+    *ponteiro = 200;
+}
+
+int main(){
+
+    int valor =32;
+    printf("O valor da variavel é %d. o endereço é %p\n", valor, &valor);
+    alterar(&valor);
+    printf(" o valor da variavel é %d. o endereço é %p\n", valor, &valor);
+
+
+    return 0;
+}
